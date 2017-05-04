1b6753a782b4e643ad68d4bb14d34528daea03cb
michael <michaelsking1993@gmail.com>
2017-05-04 12:47:31 -0500

    remove react-rails, add react_on_rails

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 gem 'haml'
 
-gem 'react-rails'
+gem 'react_on_rails', '~> 6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,43 +1,44 @@
-var Appointments = React.createClass({
-    getInitialState: function() {
-        return {
+class Appointments extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
             appointments: this.props.appointments,
             title: 'Team Standup Meeting',
             appt_time: 'Tomorrow at 9am'
         }
-    },
+    }
 
-    handleUserInput: function(obj) {
+    handleUserInput(obj) {
        this.setState(obj);
-    },
+    }
 
-    handleFormSubmit: function() {
-        var appointment = {title: this.state.title, appt_time: this.state.appt_time}
+    handleFormSubmit() {
+        const appointment = {title: this.state.title, appt_time: this.state.appt_time};
         $.post('/appointments',
             {appointment: appointment})
-            .done(function(data){
+            .done((data) => {
                 this.addNewAppointment(data);
-            }.bind(this));
-    },
+            });
+    }
 
-    addNewAppointment: function(appointment){
-        var appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
+    addNewAppointment(appointment){
+        const appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
         this.setState({
             appointments: appointments.sort(function(a,b){
                 return new Date(a.appt_time) - new Date(b.appt_time);
             })
         });
-    },
+    }
 
-    render: function() {
+    render() {
         return (
             <div>
                 <AppointmentForm title={this.state.title}
                                  appt_time={this.state.appt_time}
-                                  onUserInput={this.handleUserInput}
-                                    onFormSubmit = {this.handleFormSubmit} />
+                                  onUserInput={(obj) => this.handleUserInput(obj)}
+                                    onFormSubmit={() => this.handleFormSubmit()} />
                 <AppointmentsList appointments={this.state.appointments} />
             </div>
         )
     }
-});
+}
